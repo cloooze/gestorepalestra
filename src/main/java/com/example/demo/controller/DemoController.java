d4f75b9de6359dd099aed942174b11c45f205518
Braghese Andrea <a.braghese@reply.it>
2018-06-14 17:35:01 +0200

    Aggiunta Modal per salvataggio modifiche utente

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,6 +66,18 @@ public class DemoController {
 		
 		return modelAndView;
 	}
+	
+	@RequestMapping(value = "/updateUtente", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ModelAndView getDettagliUtente(@RequestParam("dettaglioUtente") User user) {
+		ModelAndView modelAndView = new ModelAndView();
+		
+		modelAndView.addObject("showTab", "new");
+		modelAndView.setViewName("gestione_utenti");
+		
+		return modelAndView;
+	}
+	
+	
 	
 //	@RequestMapping(value = "/getUserMock", method = RequestMethod.GET)
 	public User getUserMock() {
